ecabcea1d0e6d12ba3a668faea53fe509c0e37bd
Tomi Sarjamo <tomi.sarjamo@tietoevry.com>
2025-03-26 20:57:48 +0200

    Category adding in database and image uploading to Cloudinary.

--- backend/controllers/dashboard/categoryController.js
+++ backend/controllers/dashboard/categoryController.js
@@ -1,5 +1,7 @@
 import { responseReturn } from '../../utils/response.js';
 import formidable from 'formidable';
+import Category from '../../models/categoryModel.js';
+import { v2 as cloudinary } from 'cloudinary';
 
 const addCategory = async (req, res) => {
   const form = formidable();
@@ -11,6 +13,35 @@ const addCategory = async (req, res) => {
       let { image } = files;
       name = name.trim();
       const slug = name.split(' ').join('-');
+
+      cloudinary.config({
+        cloud_name: process.env.cloud_name,
+        api_key: process.env.api_key,
+        api_secret: process.env.api_secret,
+        secure: true,
+      });
+
+      try {
+        const result = await cloudinary.uploader.upload(image.filepath, {
+          folder: 'categories',
+        });
+
+        if (result) {
+          const category = await Category.create({
+            name,
+            slug,
+            image: result.url,
+          });
+          responseReturn(res, 201, {
+            category,
+            message: 'Category Added Successfully',
+          });
+        } else {
+          responseReturn(res, 404, { error: 'Image Upload File Error' });
+        }
+      } catch (error) {
+        responseReturn(res, 500, { error: 'Internal Server Error' });
+      }
     }
   });
 };
